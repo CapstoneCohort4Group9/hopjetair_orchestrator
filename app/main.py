da58cd64cd8ae2967c6f25bffab4e4afce0d3d4c
Harshith <harshit.sk@hotmail.com>
2025-07-24 16:29:55 +0530

    Enabling CORS for UI Integration

--- app/main.py
+++ app/main.py
@@ -2,11 +2,20 @@ from fastapi import FastAPI
 from .router import router
 from .graph import WorkflowFactory
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="LLM Orchestrator",
     description="Orchestrates LLM-based travel chatbot using LangGraph",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Initialize the workflow factory and run setup on startup
